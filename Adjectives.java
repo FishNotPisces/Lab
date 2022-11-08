7c6505ca83b67d995b494e83dce4518e61477d03
Riccardo <117733174+FishNotPisces@users.noreply.github.com>
2022-11-08 21:36:31 +0100

    Add files via upload

---
+++ Adjectives.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Adjectives {
+    public static void main (String args[]) {
+    Scanner input = new Scanner(System.in);
+    
+    String _adj = input.nextLine();
+    
+    char ending = _adj.charAt(_adj.length()-1);
+    
+    String adj = _adj.substring(0, _adj.length()-1);
+    
+    String suffix_dim = "in";
+    String suffix_sup = "issim";
+    
+    System.out.println(_adj);
+    System.out.println(adj + suffix_dim + ending);
+    System.out.println(adj + suffix_sup + ending);
+    
+    
+    }
+
+}
