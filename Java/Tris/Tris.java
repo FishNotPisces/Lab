116c80719468de80ad8256794b101d73152beef7
Riccardo <pesce.riccardo.03@gmail.com>
2022-12-20 16:12:17 +0100

    .

--- Java/Tris/Tris.java
+++ Java/Tris/Tris.java
@@ -11,17 +11,24 @@ public class Tris {
         board.printBoard();
         while (!endgame) {
             byte pos;
+            board.printTurn();
             do {
                 pos = in.nextByte();
             }while(!board.isPosEmpty(pos));
             board.getNextPos(pos);
             board.printBoard();
-            if (board.isLastTurn()) endgame = true; 
+            if (board.isLastTurn()) {
+                endgame = true;
+                System.out.println("It's a tie");
+            }
+            if (board.doWeHaveWinner()) {
+                endgame = true;
+                board.printWinner();
+            } 
             
         }
         in.close();
     }
-
 }
 
 class Board {
@@ -32,15 +39,20 @@ class Board {
     }
 
     public void printBoard() {
-        for (int i=0; i<9; i+=3) {
-            System.out.println("\n" + this.getCharAtPos(i) + " | " + this.getCharAtPos(i+1) + " | " + this.getCharAtPos(i+2));
+        for (byte i=0; i<9; i+=3) {
+            System.out.println("\n" + this.getCharAtPos(i) + " | " + this.getCharAtPos((byte)(i+1)) + " | " + this.getCharAtPos((byte)(i+2)));
             if (i==0 || i==3) System.out.print("---------");
         }
-
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+    
+    public void printTurn() {
         System.out.println("Turn: Player " + (turn ? 2 : 1));
     }
 
-    public void getNextPos(int pos) {
+    public void getNextPos(byte pos) {
         this.values[pos-1] = (byte)(turn ? 2 : 1);
         turn = !turn;
         turnCount++;
@@ -50,16 +62,37 @@ class Board {
         return turnCount > 9;
     }
 
-    public boolean isPosEmpty(int pos) {
+    public boolean isPosEmpty(byte pos) {
         return this.values[pos-1] != 1 && this.values[pos-1] != 2;
     }
 
-    public void winCon() {
-        if (turnCount < 5) return;
-        
+    public boolean doWeHaveWinner() {
+        if (turnCount < 5) return false;
+
+        if (values[0] != 0 && (values[1] == values[0] && values[1] == values[2] || values[3] == values[0] && values[3] == values[6])) {
+            return true;
+        }
+        if (values[4] != 0 && (values[4] == values[3] && values[4] == values[5] || values[4] == values[1] && values[4] == values[7])) {
+            return true;
+        }
+        if (values[8] != 0 && (values[7] == values[6] && values[7] == values[8] || values[5] == values[2] && values[5] == values[8])) {
+            return true;
+        }
+
+        if (values[4] != 0 && (values[0] == values[4] && values[4] == values[8] || values[2] == values[4] && values[4] == values[6])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void printWinner() {
+        if (doWeHaveWinner() || turnCount < 9) {
+            System.out.println("The winner is: " + (turn ? 1 : 2));
+        }
     }
     
-    private char getCharAtPos(int pos) {
+    private char getCharAtPos(byte pos) {
         switch(this.values[pos]) {
             case 1:
                 return 'x';
