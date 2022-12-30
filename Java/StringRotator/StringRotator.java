c8bcd94ef7f0f5048b1afcc9043ffa7c2a0204c7
Riccardo <pesce.riccardo.03@gmail.com>
2022-12-30 15:18:59 +0100

    .

--- Java/StringRotator/StringRotator.java
+++ Java/StringRotator/StringRotator.java
@@ -8,12 +8,20 @@ public class StringRotator {
         String word = in.nextLine();
         int amount = in.nextInt();
         in.close();
-        System.out.println(rotateString(word, amount));
+        System.out.println(rotateString_v2(word, amount));
     }
 
     public static String rotateString(String s, int amount) {
         String z = s + s + s;
         s = z.substring(s.length()-amount%s.length(), s.length()-amount%s.length()+s.length());
         return s;
+    }
+
+    public static String rotateString_v2(String s, int amount) {
+        String z="";
+        for (int i=s.length(); i<2*s.length(); i++) {
+            z += s.charAt((i-amount%s.length())%s.length());
+        }
+        return z;
     }
 }
